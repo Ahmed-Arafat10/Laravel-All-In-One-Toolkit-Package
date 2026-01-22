ed019c904ab7daa9950973cbb2ca5ba23b989aff
Ahmed-Arafat10 <ahmedxarafat0101@gmail.com>
2026-01-22 08:09:15 +0200

    update AllInOneServiceProvider & remove vendor folder from git

--- src/AllInOneServiceProvider.php
+++ src/AllInOneServiceProvider.php
@@ -5,8 +5,6 @@ namespace AhmedArafat\AllInOne;
 use AhmedArafat\AllInOne\Console\DatabaseInitialSeedersCommand;
 use AhmedArafat\AllInOne\Console\GitCommand;
 use AhmedArafat\AllInOne\Middleware\JwtMiddleware;
-
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 
@@ -14,24 +12,16 @@ class AllInOneServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->commands([
-            GitCommand::class,
-            DatabaseInitialSeedersCommand::class,
-        ]);
-        Route::aliasMiddleware(
-            'jwt',
-            JwtMiddleware::class
-        );
-        $this->publishes([
-            __DIR__ . '/Traits' => app_path('Traits'),
-            __DIR__ . '/Helpers' => app_path('Helpers'),
-            __DIR__ . '/Console' => app_path('Console/Commands/'),
-            __DIR__ . '/Exceptions' => app_path('Exceptions'),
-            __DIR__ . '/Middleware' => app_path('Http/Middleware'),
-        ], 'ahmed-arafat/all-in-one');
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                GitCommand::class,
+                DatabaseInitialSeedersCommand::class,
+            ]);
+        }
+        $this->app['router']->aliasMiddleware('jwt', JwtMiddleware::class);
     }
-    public function register()
+    public function register(): void
     {
-        parent::register();
+        // bindings, config merge, singletons
     }
 }
